86a2d3f63f77858333dc603092a1c9c9c5f8c3ab
William Scott <wscott@amplify.com>
2015-01-11 19:03:51 -0500

    Update emacs-prelude personal config.

--- prelude-personal/wds.el
+++ prelude-personal/wds.el
@@ -1,49 +1,34 @@
-;; Load solarized theme
-(disable-theme 'zenburn)
-(color-theme-solarized-dark)
+;;; wds.el --- Customizations
+;;; Commentary:
+;;; Code:
 
-;; Fix mac keys
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'super)
-(setq ns-command-modifier (quote meta))
+(setq-default default-directory "/Users/wscott")
+;(setq command-line-default-directory "~")
 
-;; No scrollbars
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(load-theme 'leuven t)
+(ido-mode t)
+(electric-pair-mode t)
 
-;; Turn off whitespace mode, flyspell
-(add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
-(add-hook 'prog-mode-hook 'flyspell-mode 0)
-(remove-hook 'message-mode-hook 'prelude-turn-on-flyspell)
-(remove-hook 'text-mode-hook 'prelude-turn-on-flyspell)
+(setq prelude-flyspell nil)
+(setq prelude-whitespace nil)
 
-;; Use Inconsolata font
-(set-frame-font "Ubuntu Mono-14")
-
-;; require final new line
-(setq require-final-newline t)
-
-;; delete trailing whitespace on save
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; use visual bell, no beeps!
-(setq visible-bell t)
-
-;; Start server
 (server-start)
 
-(add-to-list 'load-path "~/.emacs.d/personal/scala-emacs")
-(require 'scala-mode-auto)
+(set-frame-font "Menlo-14")
+(setq visible-bell t)
 
-;;(add-hook 'scala-mode-hook
-;;          '(lambda ()
-;;             (scala-mode-feature-electric-mode)
-;;             ))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq ns-command-modifier (quote meta))
 
-(require 'scala-mode)
-(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-(add-to-list 'load-path "~/.emacs.d/personal/ensime_2.9.2-0.9.8.1/elisp")
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(setq require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(push "~/lib/scala/bin/" exec-path)
-(push "~/bin" exec-path)
+(global-linum-mode 1)
+
+(custom-set-variables
+ '(ido-enable-flex-matching t)
+ )
+
+
+(provide 'wds)
+;;; wds.el ends here
