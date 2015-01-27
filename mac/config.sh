984a15f5ea6a5553668875492ec018ced281bdb1
William Scott <wscott@amplify.com>
2015-01-27 10:01:16 -0500

    Add osx config.

---
+++ mac/config.sh
@@ -0,0 +1,8 @@
+defaults write com.apple.Dock autohide-time-modifier -float 0.0
+defaults write com.apple.Dock expose-animation-duration -float 0.0
+defaults write com.apple.Dock autohide-delay -float 0.0
+defaults write com.apple.Dock tilesize -int 16
+
+killall Dock
+
+defaults write com.apple.universalaccess reduceTransparency -bool true
