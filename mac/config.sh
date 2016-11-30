74f580af7c26e20cd834b85a3c87746c5efe7812
William Scott <wds@williamscott.me>
2016-11-30 09:13:07 -0500

    Add changes from work machine

--- mac/config.sh
+++ mac/config.sh
@@ -1,3 +1,5 @@
+# https://github.com/herrbischoff/awesome-osx-command-line
+
 defaults write com.apple.Dock autohide-time-modifier -float 0.0
 defaults write com.apple.Dock expose-animation-duration -float 0.0
 defaults write com.apple.Dock autohide-delay -float 0.0
@@ -6,3 +8,4 @@ defaults write com.apple.Dock tilesize -int 16
 killall Dock
 
 defaults write com.apple.universalaccess reduceTransparency -bool true
+sudo spctl --master-disable  # Allow running apps from Anywhere
