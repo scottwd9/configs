e3a94b85af4555b60d8d9ba50e2ee8dcd9de3389
William Scott <scottwd9@gmail.com>
2017-03-17 10:15:16 -0600

    Install VSCode packages

--- mac/vscode/setup.sh
+++ mac/vscode/setup.sh
@@ -12,5 +12,18 @@ createCodeSymlink() {
     createSymlink `pwd`/$1 ~/Library/Application\ Support/Code/User/$1
 }
 
+installExtension() {
+    code --install-extension $1
+}
+
 createCodeSymlink settings.json
 createCodeSymlink keybindings.json
+
+installExtension 2gua.rainbow-brackets
+installExtension EditorConfig.EditorConfig
+installExtension dbaeumer.vscode-eslint
+installExtension dzannotti.vscode-babel-coloring
+installExtension eg2.vscode-npm-script
+installExtension oderwat.indent-rainbow
+installExtension shinnn.stylelint
+installExtension xabikos.JavaScriptSnippets
