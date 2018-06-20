b5717fcdf83638a1bd000771991d8ac79602dc6f
William Scott <scottwd9@gmail.com>
2018-06-20 16:51:21 -0400

    Set maxfilesperprocess, maxprocesses

--- mac/setup.sh
+++ mac/setup.sh
@@ -8,3 +8,9 @@ sudo pmset -a hibernatemode 0  # Disable sleep image
 sudo rm /var/vm/sleepimage
 
 sudo cp noatime.plist /Library/LaunchDaemons/noatime.plist # Disable file access time (small performance improvement?)
+
+sudo cp limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
+
+sudo cp limit.maxproc.plist /Library/LaunchDaemons/limit.maxproc.plist
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxproc.plist
